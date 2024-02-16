78faed03615c937c837aa85eb009e983f4c9a2fc
PIRANY <121549049+PIRANY1@users.noreply.github.com>
2024-02-16 18:56:28 +0100

    #6 Added More Structure

--- main.bat
+++ main.bat
@@ -1,17 +1,51 @@
-@setlocal enableextensions ENABLEDELAYEDEXPANSION 
-@if not defined debug_assist (@ECHO OFF) else (@echo on)
-rem Checks if the Script is started by a debug Script
-if not defined devtools (goto nodev) else (goto dtd)
-rem Presets the Elevated Variable when called with call
-set "elevated=0"
+:: Use only under MIT License
+:: If you want to Publish a modified Version please mention the Original Creator PIRANY and link the GitHub Repo
 
+:: Some Startup Stuff (yk)
+@echo off
+set "scriptstart=%time%"
+@title Starting Up...
+echo Starting Up...
+@color 02
+@setlocal enableextensions ENABLEDELAYEDEXPANSION 
+
+:: Checks if the Script is started by a debug Script
+:: update part to be linked here
+@if defined debug_assist @ECHO ON
+@if defined devtools goto devpart
+
+:: Allows ASCII Art to be Used in the Script (50/50 pasted) Instead of Echo Use %$Echo% " 
+SETLOCAL EnableDelayedExpansion
+SET $Echo=FOR %%I IN (1 2) DO IF %%I==2 (SETLOCAL EnableDelayedExpansion ^& FOR %%A IN (^^^!Text:""^^^^^=^^^^^"^^^!) DO ENDLOCAL ^& ENDLOCAL ^& ECHO %%~A) ELSE SETLOCAL DisableDelayedExpansion ^& SET Text=
+SETLOCAL DisableDelayedExpansion
+
+:: Presets some Variables when used in call
+set "elevated="
+set "exitcode="
+set "gitver12=v0"
+goto main
+
+:: Checks if the Script is elevated when called
 :checkelev
-rem Checks if the Script is elevated when called
 net session >nul 2>&1
 if %errorLevel% == 0 ( set "elevated=1" )
 goto :EOF
 
-:nodev
-call :checkelev
-echo %elevated%
-pause
+:: Main Part
+:main
+set "startupendtime=%time%"
+set /a res= %startupendtime% - %scriptstart% 
+@title OpenVPN-CLI-Tool
+echo             [1] Info
+echo.         
+echo             [2] Add to Path
+echo.            
+echo             [3] Autostart
+echo.            
+echo             [4]
+echo.            
+echo             [5] Check for Updates
+echo.            
+echo             [6] Exit
+
+exit /b 0
--- test.bat
+++ test.bat
@@ -1,17 +1,73 @@
-@setlocal enableextensions ENABLEDELAYEDEXPANSION 
-@if not defined debug_assist (@ECHO OFF) else (@echo on)
-rem Checks if the Script is started by a debug Script
-if not defined devtools (goto nodev) else (goto dtd)
-rem Presets the Elevated Variable when called with call
-set "elevated=0"
+:: checks for updates in a non existent settings textfile :/
+:under-construction
+echo Checking for Data...
+setlocal enabledelayedexpansion
+set "batchfile=settings.txt"
+set "linenumber=4"
+set "searchtext=uy"
 
-:checkelev
-rem Checks if the Script is elevated when called
-net session >nul 2>&1
-if %errorLevel% == 0 ( set "elevated=1" )
-goto :EOF
+for /f "tokens=1* delims=:" %%a in ('findstr /n "^" "%batchfile%"') do (
+    if %%a equ %linenumber% (
+        set "line=%%b"
+        set "line=!line:*:=!"
+        if "!line!" equ "%searchtext%" (
+            goto gitvercheck
+        ) else (
+            goto ulttop
+        )
+    )
+)
 
-:nodev
-call :checkelev
-echo %elevated%
-pause
+:gitvercheck
+set "api_url=https://api.github.com/repos/PIRANY1/openvpn-cli-tools/releases/latest"
+echo Fetching Git Url....
+@ping -n 1 localhost> nul
+echo Calling Github API...
+for /f "usebackq tokens=*" %%i in (`curl -s %api_url% ^| jq -r ".tag_name"`) do (set "latest_version=%%i")
+echo Version:%latest_version%
+if %latest_version% equ v2.6 (goto UpToDate) else (goto gitverout)
+
+:UpToDate
+@ping -n 1 localhost> nul
+echo The Version you are currently Using is the newest one (%latest_version%)
+goto ulttop
+
+
+:gitverout
+echo.
+@ping -n 1 localhost> nul
+echo Version Outdated!
+@ping -n 1 localhost> nul
+echo.
+@ping -n 1 localhost> nul
+echo Please consider downloading the new Version. 
+@ping -n 1 localhost> nul
+echo The Version you are currently using is %gitver12%
+@ping -n 1 localhost> nul 
+echo The newest Version avaiable is %latest_version%
+@ping -n 1 localhost> nul
+echo.
+echo [1] Update
+@ping -n 1 localhost> nul
+echo.
+@ping -n 1 localhost> nul
+echo [2] Continue Anyways
+@ping -n 1 localhost> nul
+echo.
+@ping -n 1 localhost> nul
+set /p menu4=Choose an Option from Above:
+If %menu4% == 1 goto gitupt
+If %menu4% == 2 goto ulttop
+goto gitverout
+
+:gitupt
+set gitverold=%gitver12%
+cd %~dp0
+set "source_dir_start=%cd%"
+cd ..
+mkdir %latest_version%
+cd %latest_version%
+git clone https://github.com/PIRANY1/DataSpammer %cd%
+echo Downloaded
+set "setupaftgitcl=1"
+main.bat
